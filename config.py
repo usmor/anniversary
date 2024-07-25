b4ba474354eff3a117f879fb44fc3608549c2f62
Futyn-Maker <github@yakuboy.ru>
2024-07-25 12:59:03 +0300

    Move uploads folder to instance

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'ANNIVERSARY_DATABASE_URL') or 'sqlite:///smoltest_3.db'
-    UPLOAD_FOLDER = 'uploads'
+    UPLOAD_FOLDER = 'instance/uploads'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

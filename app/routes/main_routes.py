d02443d06c8ba2424a4bc1e77e36c0b5ec88adb7
Zlovoblachko <dasha.kh18@gmail.com>
2024-07-30 04:58:47 +0300

    prefilled statuslist v1

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, render_template
-
+from app.models.models import StatusList
+from app import db
 
 main_routes = Blueprint('main_routes', __name__)
 
 
 @main_routes.route('/')
 def main():
+    if StatusList.query.first() is None:
+        statuses = ['Бакалавр', 'Магистрант', 'Аспирант', 'Преподаватель', 'Менеджер', 'Лаборант']
+        for stat in statuses:
+            new_status = StatusList(status=stat)
+            db.session.add(new_status)
+        db.session.commit()
     return render_template('main.html')
 
 

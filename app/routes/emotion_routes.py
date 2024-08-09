9076e72d462ec8ae24404422b629263b71d87764
usmor <113455951+usmor@users.noreply.github.com>
2024-08-09 15:29:56 +0200

    updated the emotional questionnaire

--- app/routes/emotion_routes.py
+++ app/routes/emotion_routes.py
@@ -3,11 +3,15 @@ from flask import Blueprint, render_template, redirect, url_for, request
 
 emotion_routes = Blueprint('emotion_routes', __name__)
 
+
 @emotion_routes.route('/emotion/page_<string:page_id>')
 def questionnaire(page_id):
     page_templates = {
         'personal_data': 'personal_data.html',
-        'emotion_data': 'emotion_data.html',
+        'emotion_data_1': 'emotion_data_1.html',
+        'emotion_data_2': 'emotion_data_2.html',
+        'emotion_data_3': 'emotion_data_3.html',
+        'emotion_data_4': 'emotion_data_4.html',
         'thanks': 'thanks.html'
     }
     template = page_templates.get(page_id)
@@ -29,20 +33,35 @@ def emotion_personal_data():
     second_name = request.form.get('second_name')
 
     return redirect(url_for('emotion_routes.questionnaire',
-                            page_id='emotion_data'))
+                            page_id='emotion_data_1'))
 
 
-@emotion_routes.route('/emotion_data', methods=['POST'])
-def emotion_data():
+@emotion_routes.route('/emotion_data_1', methods=['POST'])
+def emotion_data_1():
     memories_education = request.form.get('memories-education')
     memories_life = request.form.get('memories-life')
     memories_people = request.form.get('memories-people')
     significant_spaces = request.form.get('significant-spaces')
+
+    return redirect(url_for('emotion_routes.questionnaire', page_id='emotion_data_2'))
+
+
+@emotion_routes.route('/emotion_data_2', methods=['POST'])
+def emotion_data_2():
     unusual_experience = request.form.get('unusual-experience')
+    return redirect(url_for('emotion_routes.questionnaire', page_id='emotion_data_3'))
+
+
+@emotion_routes.route('/emotion_data_3', methods=['POST'])
+def emotion_data_3():
     noun = request.form.get('noun')
     verb = request.form.get('verb')
     pronoun = request.form.get('pronoun')
     school_definition = request.form.get('school-definition')
-    greetings = request.form.get('greetings')
+    return redirect(url_for('emotion_routes.questionnaire', page_id='emotion_data_4'))
 
+
+@emotion_routes.route('/emotion_data_4', methods=['POST'])
+def emotion_data_4():
+    greetings = request.form.get('greetings')
     return redirect(url_for('emotion_routes.questionnaire', page_id='thanks'))

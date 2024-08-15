3e112480fc4269ffd3f3d9aafc60b16f6d083b15
Futyn-Maker <github@yakuboy.ru>
2024-08-15 18:44:04 +0300

    Move config to env variables and add dotenv

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
+    SECRET_KEY = os.environ.get('ANNIVERSARY_SECRET_KEY') or os.urandom(32)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'ANNIVERSARY_DATABASE_URL') or 'sqlite:///smoltest_3.db'
+        'ANNIVERSARY_DATABASE_URL') or 'sqlite:///anniversary.db'
     UPLOAD_FOLDER = 'instance/uploads'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+HOST = os.environ.get('ANNIVERSARY_HOST') or '0.0.0.0'
+PORT = os.environ.get('ANNIVERSARY_PORT') or 5001
--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
 from app import create_app
+from config import HOST, PORT
 
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5001)
+    app.run(host=HOST, port=PORT, debug=False)
